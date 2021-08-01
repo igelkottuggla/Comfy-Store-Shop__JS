edc95c7313eaaddb6b847206a94c81e0b07f0582
NataliaSova <rot.fuchslein@gmail.com>
2021-08-01 15:48:40 +0500

    Navbar, Hero, Sidebar, cart - html structure / sidebar, cart toggle js

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,21 @@
 import { getElement } from '../utils.js';
 
-export const openCart = () => {};
+const nav = getElement('.nav-center');
+
+if (nav) {
+  const openCartBtn = getElement('.toggle-cart');
+  const closeCartBtn = getElement('.cart-close');
+  const cartOverlay = getElement('.cart-overlay');
+
+  openCartBtn.addEventListener('click', () => {
+    cartOverlay.classList.add('show');
+  });
+
+  closeCartBtn.addEventListener('click', () => {
+    cartOverlay.classList.remove('show');
+  });
+}
+
+export const openCart = () => {
+  cartOverlay.classList.add('show');
+};
--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,17 @@
 import { getElement } from './utils.js';
+
+const nav = getElement('.nav-center');
+
+if (nav) {
+  const toggleNav = getElement('.toggle-nav');
+  const sidebarOverlay = getElement('.sidebar-overlay');
+  const closeBtn = getElement('.sidebar-close');
+
+  toggleNav.addEventListener('click', () => {
+    sidebarOverlay.classList.add('show');
+  });
+
+  closeBtn.addEventListener('click', () => {
+    sidebarOverlay.classList.remove('show');
+  });
+}
